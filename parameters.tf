d30e3c952f967c6a01149490055fab452f07bcb6
Nilnuwan Sankalpa <nilnuwans@jkintranet.com>
2025-04-23 15:10:20 +0530

    revert second attempt

--- parameters.tf
+++ parameters.tf
@@ -1,41 +1,34 @@
 variable "resource_group_name" {
-  type        = string
   description = "Resource group name"
+  type        = string
 }
 
 variable "region" {
-  type        = string
   description = "Azure region"
+  type        = string
 }
 
 variable "vm_name" {
-  type        = string
   description = "Name of the existing virtual machine"
+  type        = string
 }
 
 variable "disk_count" {
-  type        = number
   description = "Number of data disks to attach"
-  #default     = 1
+  type        = number
 }
 
 variable "disk_size_gb" {
-  type        = number
   description = "Size of each disk in GB"
-  #default     = 128
+  type        = number
 }
 
 variable "disk_type" {
-  type        = string
   description = "Type of the managed disk (e.g. Standard_LRS, Premium_LRS)"
-  #default     = "Standard_LRS"
+  type        = string
 }
-
 
 variable "disk_names" {
   description = "List of disk names"
   type        = list(string)
 }
-
-
-
