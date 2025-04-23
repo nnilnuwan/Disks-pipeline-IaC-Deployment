698741f6b79f3703f99877382c4e5b5b3811f66a
Nilnuwan Sankalpa <nilnuwans@jkintranet.com>
2025-04-23 10:02:02 +0530

    count

--- main.tf
+++ main.tf
@@ -11,20 +11,18 @@ data "azurerm_virtual_machine" "vm" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-resource "azurerm_managed_disk" "data_disks" {
-  count               = var.disk_count
-  name                = "${var.vm_name}-data-disk-${count.index}"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+resource "azurerm_managed_disk" "data_disk" {
+  name                 = "${var.vm_name}-datadisk-0${var.disk_count}"
+  location             = data.azurerm_resource_group.rg.location
+  resource_group_name  = data.azurerm_resource_group.rg.name
   storage_account_type = var.disk_type
-  create_option       = "Empty"
-  disk_size_gb        = var.disk_size_gb
+  create_option        = "Empty"
+  disk_size_gb         = var.disk_size_gb
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "disk_attachment" {
-  count              = var.disk_count
-  managed_disk_id    = azurerm_managed_disk.data_disks[count.index].id
+  managed_disk_id    = azurerm_managed_disk.data_disk.id
   virtual_machine_id = data.azurerm_virtual_machine.vm.id
-  lun                = count.index
+  lun                = 0
   caching            = "ReadWrite"
 }
